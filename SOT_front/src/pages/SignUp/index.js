b5fad1024378c8c949711c14739a1170865c666d
이윤민 <dbsalsdl5@naver.com>
2020-10-27 21:55:47 +0900

    S03P31D208-69 | 회원가입 페이지 구현중

--- SOT_front/src/pages/SignUp/index.js
+++ SOT_front/src/pages/SignUp/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React , { useState } from 'react';
 import {
   View,
   Text,
+  TextInput,
   Button,
 } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 const SignUp = ({navigation}) => {
+
+    const [Search, setSearch] = useState("");
+
+    const onSearchHandler = (Search) => {
+        setSearch({Search});
+    }
+
     return (
         <View>
-            <Text>회원가입 화면</Text>
+            <Text>회원가입</Text>
+            <TextInput placeholder="아이디"></TextInput>
+            <TextInput placeholder="비밀번호" secureTextEntry={true}></TextInput>
+            <SearchBar placeholder="학교명 검색" onChangeText={onSearchHandler} value={Search} lightTheme={true}></SearchBar>
+
             <Button title="메인으로" onPress={() => navigation.navigate('Start') }></Button>
         </View>
     );
--- SOT_front/src/pages/Start/index.js
+++ SOT_front/src/pages/Start/index.js
@@ -10,8 +10,18 @@ import 'react-native-gesture-handler';
 const Start = ({navigation}) => {
     return (
         <View>
-            <Text>시작 화면</Text>
-            <Button title="회원가입화면으로" onPress={ () => navigation.navigate('SignUp') }></Button>
+            <View>
+                <Text>SOT</Text>
+            </View>
+            <View>
+                <TextInput placeholder="아이디"></TextInput>
+                <TextInput placeholder="비밀번호" secureTextEntry={true}></TextInput>
+            </View>
+            <View>
+                <Button title="회원가입" onPress={ () => navigation.navigate('SignUp') }></Button>
+                <Button title="로그인"></Button>
+            </View>
+            
         </View>
     );
 };
