a172bf5513dc7f1b182d8b036b599fc78dc664d0
이윤민 <dbsalsdl5@naver.com>
2020-10-28 22:22:55 +0900

    S03P31D208-66 | 네비 중첩

--- SOT_front/App.js
+++ SOT_front/App.js
@@ -10,7 +10,6 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Start from './src/pages/Start';
 import SignUp from './src/pages/SignUp';
-import Main from './src/pages/Main';
 import Home from './src/pages/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -24,7 +23,7 @@ function MyStack() {
     <Stack.Navigator>
       <Stack.Screen
         name="Start"
-        options={{headerShown: true}}
+        options={{headerShown: false}}
         component={Start}
       />
       <Stack.Screen
@@ -34,8 +33,8 @@ function MyStack() {
       />
       <Stack.Screen
         name="Main"
-        options={{headerShown: true}}
-        component={Main}
+        options={{headerShown: false}}
+        component={TabsScreen}
       />
     </Stack.Navigator>
   );
@@ -59,48 +58,53 @@ function SettingsScreen() {
 
 const Tab = createBottomTabNavigator();
 
+const TabsScreen = () => (
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        if (route.name === '홈') {
+          return (
+            <Icon
+              name={focused ? 'home' : 'home-outline'}
+              size={size}
+              color={color}
+            />
+          );
+        } else if (route.name === '게시판 목록') {
+          return (
+            <Icon
+              name={focused ? 'newspaper' : 'newspaper-outline'}
+              size={size}
+              color={color}
+            />
+          );
+        } else if (route.name === '게임') {
+          return (
+            <Icon
+              name={focused ? 'game-controller' : 'game-controller-outline'}
+              size={size}
+              color={color}
+            />
+          );
+        }
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}>
+    <Tab.Screen name="홈" component={Home} />
+    <Tab.Screen name="게시판 목록" component={SettingsScreen} />
+    <Tab.Screen name="게임" component={HomeScreen} />
+  </Tab.Navigator>
+);
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            if (route.name === '홈') {
-              return (
-                <Icon
-                  name={focused ? 'home' : 'home-outline'}
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === '게시판 목록') {
-              return (
-                <Icon
-                  name={focused ? 'newspaper' : 'newspaper-outline'}
-                  size={size}
-                  color={color}
-                />
-              );
-            } else if (route.name === '게임') {
-              return (
-                <Icon
-                  name={focused ? 'game-controller' : 'game-controller-outline'}
-                  size={size}
-                  color={color}
-                />
-              );
-            }
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen name="홈" component={Home} />
-        <Tab.Screen name="게시판 목록" component={SettingsScreen} />
-        <Tab.Screen name="게임" component={HomeScreen} />
-      </Tab.Navigator>
-      {/* <MyStack /> */}
+      
+      <MyStack />
+
     </NavigationContainer>
   );
 }
