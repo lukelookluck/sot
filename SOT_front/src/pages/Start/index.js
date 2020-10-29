1c8abf1c7f832351597e2188fe0d714737a23b15
Luke <gunhee9177@naver.com>
2020-10-29 13:22:42 +0900

    S03P31D208-56 즐찾 게시판 1차 디자인

--- SOT_front/src/pages/Start/index.js
+++ SOT_front/src/pages/Start/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {
-  View,
-  Text,
-  TextInput,
-  Button,
+    View,
+    Text,
+    TextInput,
+    Button,
 } from 'react-native';
 import 'react-native-gesture-handler';
 
-const Start = ({navigation}) => {
+const Start = ({ navigation }) => {
     return (
         <View>
             <View>
@@ -18,10 +18,10 @@ const Start = ({navigation}) => {
                 <TextInput placeholder="비밀번호" secureTextEntry={true}></TextInput>
             </View>
             <View>
-                <Button title="회원가입" onPress={ () => navigation.navigate('SignUp') }></Button>
-                <Button title="로그인" onPress={ () => navigation.navigate('Main') }></Button>
+                <Button title="회원가입" onPress={() => navigation.navigate('SignUp')}></Button>
+                <Button title="로그인" onPress={() => navigation.navigate('Main')}></Button>
             </View>
-            
+
         </View>
     );
 };
