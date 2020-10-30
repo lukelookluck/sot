39e70815f26a992f280ed1ff06562c220bf4b778
이윤민 <dbsalsdl5@naver.com>
2020-10-30 21:36:33 +0900

    create List

--- SOT_front/src/pages/List/index.js
+++ SOT_front/src/pages/List/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, FlatList, StyleSheet} from 'react-native';
 
-export default function List() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const List = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View>
       <Text>게시판 목록 탭</Text>
+
+      <FlatList
+        data={[
+          {key: '자유 게시판'},
+          {key: '1학년 게시판'},
+          {key: '2학년 게시판'},
+          {key: '3학년 게시판'},
+        ]}
+        renderItem={({item}) => (
+          <Text style={styles.item}>{item.key}</Text>
+        )}></FlatList>
+        
     </View>
   );
-}
+};
+
+export default List;
