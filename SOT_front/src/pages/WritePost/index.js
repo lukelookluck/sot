d724e2b262a5a8755816fadd6a4a4759f33c1756
이윤민 <dbsalsdl5@naver.com>
2020-11-03 22:32:18 +0900

    S03P31D208-77 | 게시글 작성 페이지

--- SOT_front/src/pages/WritePost/index.js
+++ SOT_front/src/pages/WritePost/index.js
@@ -22,15 +22,16 @@ const WritePost = ({navigation}) => {
           <TextInput placeholder="제목"></TextInput>
         </View>
 
-        <View>
+        <View style={styles.detailbox}>
           <TextInput placeholder="내용"></TextInput>
         </View>
 
-        <View>
-          <TouchableOpacity onPress={addPost}>
-            <Text>글 작성</Text>
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity onPress={addPost} style={styles.btn}>
+            <Text style={{fontSize: 18, color: 'white'}}>글 작성</Text>
           </TouchableOpacity>
         </View>
+
       </KeyboardAwareScrollView>
     </ScrollView>
   );
@@ -38,7 +39,18 @@ const WritePost = ({navigation}) => {
 
 const styles = StyleSheet.create({
 
+  detailbox: {
+    height: 300,
+  },
 
+  btn: {
+    width: 90,
+    height: 40,
+    backgroundColor: '#F14E23',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  }
 });
 
 export default WritePost;
