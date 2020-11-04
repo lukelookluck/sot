965d58b8e90790801fb02e4539c0bf1b4f0c435c
이윤민 <dbsalsdl5@naver.com>
2020-11-04 17:19:47 +0900

    S03P31D208-69 | 연동중

--- SOT_front/src/pages/SignUp/index.js
+++ SOT_front/src/pages/SignUp/index.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import {
   View,
   Text,
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import {SearchBar, Input} from 'react-native-elements';
 import 'react-native-gesture-handler';
@@ -12,53 +14,99 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const SignUp = ({navigation}) => {
   const [Search, setSearch] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userNick, setUserNick] = useState('');
+  const [userPw, setUserPw] = useState('');
+  // const [userConPw, setUserConPw] = useState('');
+
+  const userIdHandler = (id) => {
+    setUserId(id);
+  }
+
+  const userNickHandler = (nick) => {
+    setUserNick(nick);
+  }
+
+  const userPwHandler = (pw) => {
+    setUserPw(pw);
+  }
 
   const onSearchHandler = (text) => {
     setSearch(text);
   };
 
+  const signUpHandler = () => {
+
+    console.log("클릭했어요!!!");
+
+    axios.post('http://localhost:8080/user/register/', {
+      email: userId,
+      nickname: userNick,
+      password: userPw,
+      schoolId: 2,
+    })
+    .then(function (response) {
+      console.log("제대로간겨??");
+      console.log(response);
+      navigation.navigate('Start');
+    })
+    .catch(function (error) {
+      console.log("에러난겨??");
+      console.log(error);
+    });
+
+  }
+
+
   return (
-    <KeyboardAwareScrollView
-      style={styles.page}
-      scrollEnabled={true}
-      contentContainerStyle={styles.screen}>
-      
-      <View style={styles.box}>
-        <Input placeholder="ex) sot2020" label="아이디를 입력해주세요"></Input>
-        <Input placeholder="비밀번호" label="비밀번호를 입력해주세요" secureTextEntry={true}></Input>
-        <Input placeholder="비밀번호 확인" label="비밀번호를 다시 입력해주세요" secureTextEntry={true}></Input>
-      </View>
+    <ScrollView style={{backgroundColor: '#FFFFE0'}}>
 
-      <View style={styles.box2}>
-        <SearchBar
-          placeholder="학교명 검색"
-          onChangeText={onSearchHandler}
-          value={Search}
-          style={styles.searchbar}
-          containerStyle={styles.search}
-          inputContainerStyle={styles.search2}
-          inputStyle={styles.search3}
-          ></SearchBar>
+        <KeyboardAwareScrollView
+          style={styles.page}
+          scrollEnabled={true}
+          contentContainerStyle={styles.screen}>
+          
+          <View style={styles.box}>
+            <Input placeholder="아이디" label="아이디를 입력해주세요" onChangeText={userIdHandler} value={userId}></Input>
+            <Input placeholder="닉네임" label="닉네임을 입력해주세요" onChangeText={userNickHandler} value={userNick}></Input>
+            <Input placeholder="비밀번호" label="비밀번호를 입력해주세요" secureTextEntry={true} onChangeText={userPwHandler} value={userPw}></Input>
+            {/* <Input placeholder="비밀번호 확인" label="비밀번호를 다시 입력해주세요" secureTextEntry={true}></Input> */}
+          </View>
 
-        <View style={styles.btnbox}>
-          <TouchableOpacity
-            style={styles.btn}
-            onPress={() => navigation.navigate('Start')}>
-            <Text style={styles.btntext}>회원가입</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+          <View style={styles.box2}>
+            <SearchBar
+              placeholder="학교명 검색"
+              onChangeText={onSearchHandler}
+              value={Search}
+              style={styles.searchbar}
+              containerStyle={styles.search}
+              inputContainerStyle={styles.search2}
+              inputStyle={styles.search3}
+              ></SearchBar>
 
-    </KeyboardAwareScrollView>
+            <View style={styles.btnbox}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={signUpHandler}>
+                <Text style={styles.btntext}>회원가입</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAwareScrollView>
+
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   page: {
     backgroundColor: '#FFFFE0',
+    marginTop: 40,
+    marginBottom: 40,
+    // flex: 1,
   },
   screen: {
-    flex: 1,
+    // flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -106,8 +154,6 @@ const styles = StyleSheet.create({
   search3: {
     backgroundColor: '#FFFFE0',
   },
-
-
 
 });
 
