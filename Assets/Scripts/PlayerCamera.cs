861817747e29ec36d6512a279ebf22434830831c
Hong Se Jin <hongsj36@gmail.com>
2020-11-25 22:59:53 +0900

    add projectile asset && modify UI priority

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Camera playerCamera;
+
+    void Start()
+    {
+        playerCamera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zoom();
+    }
+
+    public void Zoom()
+    {
+        var scroll = Input.mouseScrollDelta;
+        playerCamera.fieldOfView = Mathf.Clamp(playerCamera.fieldOfView - scroll.y * 3f, 10f, 60f);
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<Camera>().fieldOfView = playerCamera.fieldOfView;
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator anim;
+    private Camera playerCamera;
+    
+    private int layerMask;
+    private float rotateSpeed = 10f;
+    private Rigidbody rb;
+
+
+    private Vector3 destination;
+    private bool sDown;
+
+    [SerializeField]
+    private GameObject playerCharacter;
+
+    public bool isStun;
+    public bool isDestination;
+    public bool isMove;
+    public float moveAcc;
+
+    private void Awake()
+    {
+        playerCamera = Camera.main;
+        layerMask = 1 << LayerMask.NameToLayer("Ground");
+        anim = gameObject.GetComponentInChildren<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+    }
+
+    private void GetInput()
+    {
+        sDown = Input.GetKeyDown("s");
+
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(playerCamera.ScreenPointToRay(Input.mousePosition), out hit, 100f, layerMask))
+            {
+                destination = hit.point;
+                isDestination = true;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (sDown)
+        {
+            isDestination = false;
+        }
+        if (isStun)
+        {
+            isMove = false;
+        }
+        else
+        {
+            isMove = isDestination;
+        }
+
+        anim.SetBool("isRun", isMove);
+
+        if (isMove)
+        {
+            // Stop and reset destination
+            if (Vector3.Distance(destination, transform.position) <= Mathf.Max(rb.velocity.magnitude / 2 - 2f, 0.1f))
+            {
+                isDestination = false;
+                return;
+            }
+
+            // Find direction and AddForce
+            var dir = destination - transform.position;
+            if (rb.velocity.magnitude <= 10)
+            {
+                rb.AddForce(dir.normalized * Time.deltaTime * moveAcc, ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    private void Turn()
+    {
+        var dir = destination - transform.position;
+        if (dir != Vector3.zero && isMove)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(dir);
+            playerCharacter.transform.rotation = Quaternion.Slerp(playerCharacter.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+
+}
